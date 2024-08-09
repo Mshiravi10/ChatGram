fa56c42be5903a9f419a37f24a4977124039d2e1
Mohammad Shiravi <aminmamad98@gmail.com>
2024-08-09 14:29:17 +0330

    update client app
    add register and login jsx

---
+++ clientapp/src/components/TextSection.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TextSection() {
+  return (
+    <div className="text-container text-white text-center">
+      <h2 className="heading">
+        Connect with friends
+        <br />
+        through ChatGram
+      </h2>
+      <p className="paragraph">
+        Chat with your friends in real-time, share your thoughts, and stay connected wherever you are. Experience seamless communication with our intuitive platform.
+      </p>
+    </div>
+  );
+}
+
+export default TextSection;
